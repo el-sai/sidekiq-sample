069ed4e8c40b62187ca740afdf94e929146ccf6c
Jayanth Sai V <jayanth.sai.v@gmail.com>
2016-02-15 16:41:01 +0530

    removed unwanted gems and changed mailer method accepting id instead of object

--- app/mailers/sample_mailer.rb
+++ app/mailers/sample_mailer.rb
@@ -1,7 +1,7 @@
 class SampleMailer < ApplicationMailer
 
-	def send_email(post)
-    @post = post
-    mail(to: 'jayanth@addvalsolutions.com', subject: 'Testing background job')
+	def send_email(post_id)
+    @post = Post.find_by_id(post_id)
+    mail(to: 'jayanth@addvalsolutions.com', subject: 'Testing background job') if @post
   end
 end
